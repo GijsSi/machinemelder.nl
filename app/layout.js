65d83f702926d3f5b994fd9ebc3b3bdf9aaaad94
Gijs Siemer <gijs.siemer@me.com>
2024-08-16 16:54:50 +0200

    Added GA

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,20 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import './globals.css';
 import '@maptiler/sdk/dist/maptiler-sdk.css';
 
-import {Inter} from 'next/font/google';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({subsets: ['latin']});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Machinemelder.nl',
   description: 'Check of je flessen kunt inleveren bij de supermarkt',
 };
 
-export default function RootLayout({children}) {
-  return (<html lang = 'en'>
-          <body className = {inter.className}>{children}</body>
-    </html>);
+export default function RootLayout({ children }) {
+  return (<html lang='en'>
+    <body className={inter.className}>{children}</body>
+    <GoogleAnalytics gaId="G-7NMFHCTV7G" />
+  </html>);
 }
+
