2ce9a76fdb98d814fe22024bdbae78aae0a307d9
Gijs Siemer <gijs.siemer@me.com>
2024-08-18 17:07:32 +0200

    Added plausible

--- app/layout.js
+++ app/layout.js
@@ -3,21 +3,33 @@ import '@maptiler/sdk/dist/maptiler-sdk.css';
 
 import CookieBanner from '@/components/cookieBanner';
 import GoogleAnalytics from '@/components/GoogleAnalytics';
-import {Analytics} from '@vercel/analytics/react';
-import {Inter} from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
 
-
-const inter = Inter({subsets: ['latin']});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Machinemelder.nl',
   description: 'Check of je flessen kunt inleveren bij de supermarkt',
 };
 
-export default function RootLayout({children}) {
-  return (<html lang = 'en'>{
-      /* <GoogleAnalytics GA_MEASUREMENT_ID='G-7NMFHCTV7G' /> */}<
-          body className = {inter.className}>{children} {
-      /* <CookieBanner /> */}<Analytics /></body>
-  </html>);
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <script
+          defer
+          data-domain="machinemelder.nl"
+          src="http://161.97.151.230:8001/js/script.js"
+        ></script>
+      </Head>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
 }
