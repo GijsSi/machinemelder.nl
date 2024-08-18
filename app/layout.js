a95004df42c60d8ec16e3a01459c9512e4c70990
Gijs Siemer <gijs.siemer@me.com>
2024-08-18 17:17:29 +0200

    changed implementation

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import GoogleAnalytics from '@/components/GoogleAnalytics';
 import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
+import PlausibleProvider from 'next-plausible';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,20 +17,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <script
-          defer
-          data-domain="machinemelder.nl"
-          src="http://161.97.151.230:8001/js/script.js"
-        ></script>
-      </Head>
-      <body className={inter.className}>
-        {children}
-        <Analytics />
-      </body>
-    </html>
+    <PlausibleProvider
+      domain="machinemelder.nl"
+      customDomain="http://161.97.151.230:8001"  // Point to your self-hosted instance
+      selfHosted={true}  // Indicate that you're using a self-hosted instance
+    >
+      <html lang="en">
+        <Head>
+          <title>{metadata.title}</title>
+          <meta name="description" content={metadata.description} />
+        </Head>
+        <body className={inter.className}>
+          {children}
+          <Analytics />
+        </body>
+      </html>
+    </PlausibleProvider>
   );
 }
